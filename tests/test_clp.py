719765fa3b4682606cdd8561713111a5eeaffede
Gianluca Campanella <gianluca@campanella.org>
2017-09-05 16:38:52 +0100

    Updated unit tests

--- tests/test_clp.py
+++ tests/test_clp.py
@@ -6,16 +6,6 @@ import clpy
 
 class TestClp(unittest.TestCase):
     def test_solve(self):
-        solver = self._example()
-        solver.initial_solve()
-        np.testing.assert_equal(solver.status(), int(clpy.Status.OPTIMAL))
-        np.testing.assert_almost_equal(solver.objective_value(), 268.0)
-        np.testing.assert_almost_equal(solver.solution(), [1.8, 20.8, 1.6])
-        np.testing.assert_almost_equal(solver.reduced_costs(), np.zeros(3))
-        np.testing.assert_almost_equal(solver.shadow_prices(), [1, 6, 0, 1])
-
-    @staticmethod
-    def _example() -> clpy.Clp:
         x = np.array([
             [3, 2, 5],
             [2, 1, 1],
@@ -30,4 +20,9 @@ class TestClp(unittest.TestCase):
         solver = clpy.Clp(x, col_lb, col_ub, obj, row_lb, row_ub)
         solver.set_log_level(clpy.LogLevel.NONE)
         solver.set_optimization_direction(clpy.OptimizationDirection.MAXIMIZE)
-        return solver
+        solver.initial_solve()
+        np.testing.assert_equal(solver.status(), int(clpy.Status.OPTIMAL))
+        np.testing.assert_almost_equal(solver.objective_value(), 268.0)
+        np.testing.assert_almost_equal(solver.solution(), [1.8, 20.8, 1.6])
+        np.testing.assert_almost_equal(solver.reduced_costs(), np.zeros(3))
+        np.testing.assert_almost_equal(solver.shadow_prices(), [1, 6, 0, 1])
